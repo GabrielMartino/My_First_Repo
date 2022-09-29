a1fafdacc9b820479156e6f3d6ff8582b670ebbd
Gabriel Martino <GabrielMartino@users.noreply.github.com>
2022-09-29 21:49:31 +1000

    20.2

--- Portfolio_V2/js/index.js
+++ Portfolio_V2/js/index.js
@@ -12,3 +12,21 @@ $("button").click(function() {
       $(this).removeClass('buttonClicked');
     });
 	});
+
+
+
+ 
+  $("button").hover( function(){
+    if ($(this).children("span").hasClass("iconHidden")) {
+      $("span").removeClass("iconHidden").addClass("iconShow");
+      $("span").animate({ top: "5" }, "fast");
+      $("span").animate({ opacity: "1" }, "fast");
+    }
+    else {
+    $("span").removeClass("iconShow").addClass("iconHidden");
+    $("span").animate({ top: "-5" }, "fast");
+    }
+
+  });
+
+
